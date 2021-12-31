0c3bc46b72b671e874079a54cf7c49b6159963c4
AndroPlus <contact.androplus@gmail.com>
2021-12-31 07:02:22 +0530

    included datelibrary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DateUtilsService } from '@rameshd33/ang-lib';
 import { Observable, Subject } from 'rxjs';
 import { ShareDataService } from './services/share-data-service';
 
@@ -12,13 +13,14 @@ export class AppComponent {
   title = 'Ramesh is title';
   mSubject = new Subject()
 
-  constructor(private shareData: ShareDataService){}
+  constructor(private shareData: ShareDataService, private dateLib: DateUtilsService){}
   
 
   public addData(): void {
     this.shareData.updateShareData(Math.random()+'');
   }
   ngOnInit(): void {
+    this.title = ''+this.dateLib.isValidDate('01/11/2003'); // using @rameshd33/ang-lib
      this.mSubject.subscribe(val=>console.log(val));
     // this.mSubject.next(1);
     // this.mSubject.next(2);
